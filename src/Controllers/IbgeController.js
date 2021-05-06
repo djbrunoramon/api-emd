73f3031ba971ebc653ebed9aad38b3fdef14b64e
djbrunoramon <52329129+djbrunoramon@users.noreply.github.com>
2021-05-06 10:37:36 -0300

    Update IbgeController.js

--- src/Controllers/IbgeController.js
+++ src/Controllers/IbgeController.js
@@ -3,20 +3,16 @@ var getJSON = require('get-json')
 
 let url_estados = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
 
-
+//lista todos os estados
 exports.getEstados = (req, res, next) => {
-    
     getJSON(url_estados, function(error, response){
         res.status(200).send(response);
     })    
-
  };
-
+//lista o estado por id
  exports.getEstadosById = (req, res, next) => {
     let id = req.params.id;
-
     getJSON(url_estados+'/'+id, function(error, response){
         res.status(200).send(response);
     })    
-
  };
